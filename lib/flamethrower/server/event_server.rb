a2c02cc5d949d87c411fe67b728c8478ecabed25
Blake Smith <blakesmith0@gmail.com>
2011-04-29 20:17:40 -0500

    Pass options hash into server init

--- lib/flamethrower/server/event_server.rb
+++ lib/flamethrower/server/event_server.rb
@@ -24,11 +24,11 @@ module Flamethrower
   class EventServer
     attr_reader :host, :port, :campfire_connection, :connections
 
-    def initialize(host, port, domain, token)
-      @host = host || "0.0.0.0"
-      @port = port || 6667
-      @domain = domain
-      @token = token
+    def initialize(options = {})
+      @host = options['host'] || "0.0.0.0"
+      @port = options['port'] || 6667
+      @domain = options['domain']
+      @token = options['token']
       @connections = []
     end
 
